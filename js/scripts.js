0918333c490249d2b1ea0c813fa86f7fe90ca7bc
BRIAN MAKAU <brian.mutiso@student.moringaschool.com>
2021-09-21 15:56:23 +0300

    Add functionality for the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,57 @@
-var order = function(type,size,crust,toppings,delivery){
-    this.type = type;
-    this.sizes = sizes;
-    this.crusts = crusts;
-    this.toppings = toppings;
-    this.delivery = delivery;
-    this.number = number;
-}
-function getPrice(){
-
-    let sizeCost;
-    let crustCost;
-    let toppings;
-    let delivery;
-
-    if (this.sizes === "small"){
-        sizeCost = 600;
-    }
-    else if (this.sizes === "medium"){
-        sizeCost = 800;
-    }
-    else if(this.sizes === "large"){
-        sizeCost = 1000;
-    }
-    else if(this.sizes === "small"){
-        crustCost === 200;
-    }
-    else if(this.sizes === "medium"){
-        crustCost = 250;
-    }
-    else if (this.sizes === "large"){
-        crustCost = 300;
-    }
-    else if (this.sizes === "small"){
-        toppings = 100;
-    }
-    else if(this.sizes === "medium"){
-        toppings = 200;
-    }
-    else if(this.sizes === "large"){
-        toppings = 300;
-    }
-    else if(this.delivery === "deliver"){
-        delivery = 150;
-    }
-    else if(delivery === "pick-up"){
-        this.delivery = 0;
-    };
-
+function order(type, size, crust, toppings, delivery, number) {
+  this.type = type;
+  this.size = size;
+  this.crusts = crust;
+  this.toppings = toppings;
+  this.delivery = delivery;
+  this.number = number;
+  this.total = 0;
+  this.pizzaSize = 0;
+  this.pizzaCrustCost = 0;
+  this.toppingsPrice = 0;
+  this.deliveryPrice = 0;
 }
 
-$(document).ready(function(){
-    $(".button").submit(function(){
-        var sizes = $("");
-        var types = $("");
-        var crusts = $("");
-        var toppings = $("");
-        var number = $("");
-        var delivery = $("");
+order.prototype.makeOrder = function () {
+  if (this.size === "Small") { 
+    this.pizzaSize = 600;
+  } else if (this.size === "Medium") {
+    this.pizzaSize = 800;
+  } else if (this.size === "Large") {
+    this.pizzaSize = 1000;
+  } else if (this.size === "Small") {
+    this.pizzaCrustCost === 200;
+  } else if (this.size === "Medium") {
+    this.pizzaCrustCost = 250;
+  } else if (this.size === "Large") {
+    this.pizzaCrustCost = 300;
+  } else if (this.size === "Small") {
+    this.toppingsPrice = 100;
+  } else if (this.size === "Medium") {
+    this.toppingsPrice = 200;
+  } else if (this.size === "Large") {
+    this.toppingsPrice = 300;
+  } else if (this.delivery === "Deliver") {
+    this.deliveryPrice = 150;
+  } else if (delivery === "Pick-up") {
+    this.deliveryPrice = 0;
+  }
 
-        let price  = new order(sizeCost, crustCost,toppings,delivery);
-    });
-});
+  this.total =
+    (this.pizzaSize + this.pizzaCrustCost + this.toppingsPrice + this.deliveryPrice) * this.number;
+};
+
+function getValues() {
+  var sizes = document.getElementById("size").value;
+  var types = document.getElementById("type").value;
+  var crusts = document.getElementById("crust").value;
+  var toppings = document.getElementById("topping").value;
+  var number = document.getElementById("number").value;
+  var delivery = document.getElementById("delivery").value;
+
+  let newOrder = new order(types, sizes, crusts, toppings, delivery, parseInt(number));
+  newOrder.makeOrder();
+  window.alert(
+    `Hello You ordered ${number} ${sizes} ${types} pizza(s) with ${crusts} with ${toppings} toppings. That should amount to ${newOrder.total}`
+  );
+}
